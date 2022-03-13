f0a18715958067239a353700217dab3c78c3e022
James Morris <70540466+jleomorris@users.noreply.github.com>
2022-03-13 00:56:35 +0000

    Add test query

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -5,3 +5,26 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   uri: "http://localhost:4000/graphql",
 });
+
+// Test query
+client
+  .query({
+    query: gql`
+      query TestQuery {
+        launch(id: 56) {
+          id
+          site
+          mission {
+            name
+          }
+          rocket {
+            id
+            name
+            type
+          }
+          isBooked
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
