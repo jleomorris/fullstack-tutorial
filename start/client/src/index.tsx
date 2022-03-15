4fd6a864c23aab0a0b3a56d01f0496945785165c
James Morris <70540466+jleomorris@users.noreply.github.com>
2022-03-15 22:42:25 +0000

    Add isLoggedIn logic and component

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -3,11 +3,13 @@ import {
   NormalizedCacheObject,
   ApolloProvider,
   gql,
+  useQuery,
 } from "@apollo/client";
 import { cache } from "./cache";
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
+import Login from "./pages/login";
 import injectStyles from "./styles";
 
 export const typeDefs = gql`
@@ -29,10 +31,21 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
 
 injectStyles();
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
 // Pass the ApolloClient instance to the ApolloProvider component
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
